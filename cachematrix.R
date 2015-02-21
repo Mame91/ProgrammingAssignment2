022ae76a8dc16be3e5b449d257542befefc347b1
Mame91 <manuel.meretto@gmail.com>
2015-02-21 11:06:29 +0100

    Solution placed in the cache matrix.R
    R functions edited.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## Description of functions: two functions that cache the inverse of a matrix (benefits: reducing computation cost)
+## Assumption: matrix supplied are always invertible.
 
-## Write a short comment describing this function
+## Function 1:     makeCacheMatrix
+## Function that creates a list that contains 4 member functions: set, get, setInv and getInv. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        xinv <- NULL 
+        # Set function: set a matrix to object created by makeCacheMatrix function
+        set <- function(y) {
+                x <<- y
+                xinv <<- NULL
+        }
+        # Get function: returns the input matrix
+        get <- function() x 
+        # Set function: set the inversed matrix
+        setInv <- function(inv) xinv <<- inv
+        # Get function: returns the inversed matrix
+        getInv <- function() xinv 
+        # Return a list that contains functions previously defined
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## Function 2:     cacheSolve
+## Function that computes the inverse of the matrix returned by makeCacheMatrix function.
+## If the inverse has already been calculated and the matrix has not changed, the cachesolve retrieves
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        mat <- x$getInv() 
+        if(!is.null(mat)) {
+                message("Getting cached data")
+                return(mat)
+        }
+        data <- x$get()
+        mat <- solve(data)        # "data" is a square invertible matrix so solve(data) returns its inverse
+        x$setInv(mat)
+        mat
 }
